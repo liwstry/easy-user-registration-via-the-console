df2f435c8e26643b990cb968b9f5116a3a5ee9c5
liwstry <liwstry5@gmail.com>
2025-04-23 13:15:12 +0900

    Update class_Database.py

--- database/class_Database.py
+++ database/class_Database.py
@@ -9,16 +9,15 @@ class Database:
         self.engine = create_engine(f"sqlite:///{file_name}")
         metadata.create_all(self.engine)
         self.Session = sessionmaker(bind=self.engine)
-        
+
     def add_user(self, user_data):
         session = self.Session()
         user = User.users.insert().values(**user_data)
         session.execute(user)
         session.commit()
         session.close()
-        
+
     def data(self):
-        
+
         user_data = input_data()
         self.add_user(user_data)
-        
